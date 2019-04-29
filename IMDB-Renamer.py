f051119b3c18b3d1433092d3a1b9b5a80a07db7f
Ali Elsokary <ali.elsokary8@gmail.com>
2019-04-29 07:40:03 +0200

    Extract getting scrapped data function to smaller functions. Change the method of getting season number. Create Model for Series data.

--- IMDB-Renamer.py
+++ IMDB-Renamer.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-# *** A script to get list of episodes names from IMDB and change episode filnames in a given Directory. ***
+# *** A script to get list of episodes names from IMDB and rename the episodes files in a given Directory. ***
 
 import bs4, requests, os, shutil, re, sys, ctypes, glob
 	
@@ -10,27 +10,90 @@ userConfirmed = False
 seasonNumber = 0
 extensions = (".mkv", ".mp4", ".avi")
 # extensions = (".srt")
-# -- Initiate the request from IMDB Series Url.
-def getSeriesUrl(seriesUrl):
+
+class Series:
+	def __init__(self, name, number, episodes):
+		self.name = name
+		self.number = number
+		self.episodes = episodes
+
+def startScript():
+	print('Enter IMDB Series Url:')
+	url = input()
+	print('Downloading data from IMDB...\n')
+	returnedSoupObject = downloadSeriesDataFrom(url)
+
+	episodes = extractEpisodesNamesFrom(returnedSoupObject)
+
+	seriesName = extractSeriesNameFrom(returnedSoupObject)
+
+	sNumber = extractSeasonNumberFrom(returnedSoupObject)
+
+
+	series = Series(seriesName, sNumber, episodes)
+
+	print('Series:',series.name)
+	print('Season:',series.number)
+
+	print('These are the episodes names:\n')
+	for episode in series.episodes:
+		print(episode)
+
+	print('\n')
+	print('Press Y to Continue.')
+	confirmation = input()
+
+	if(confirmation == 'Y' or confirmation == 'y'):
+		newWorkingDirectory()
+		renameFiles('n')
+		print('Do you want to continue? Type Y to continue or N to abort')
+		rename = input()
+
+		if(rename == 'Y' or rename == 'y'):
+			renameFiles(rename)
+
+		elif(rename == 'N' or rename == 'n'):
+			print('Script Terminated.')
+			quit()
+
+		else:
+			print('Please type Y or N.')
+
+
+	elif(confirmation == 'N' or confirmation == 'n'):
+		print('Script Terminated')
+		quit()
+
+	else:
+		print('Please type Y or N.')
+
+
+def downloadSeriesDataFrom(seriesUrl):
 	res = requests.get(seriesUrl)
 	# -- Check for a successful 200.
 	res.raise_for_status()
 	soupObject = bs4.BeautifulSoup(res.text, 'html.parser')
+	return soupObject
 
+def extractSeriesNameFrom(soupObject):
+	seriesName = soupObject.find(itemprop="name")
+	seriesName = seriesName.a.get_text()
+	return seriesName
+
+def extractSeasonNumberFrom(soupObject):
+	seasonsList = soupObject.find_all(class_="seasonAndYearNav")
+	for item in seasonsList:
+		sNumber = item.find(selected="selected").get("value")
+	return sNumber
+
+def extractEpisodesNamesFrom(soupObject):
 	# -- Get list of episodes within calss "info".
 	episodes = soupObject.find_all(class_="info")
 	for episode in episodes:
 		episodeName = (episode.a.get_text())
 		episodesNames.append(episodeName)
-		# print(episodeName)
 	return episodesNames
 
-def getSeasonNumber(url):
-	seasonRegex = re.compile(r'(?<==)[^.]?\d')
-	matchObject = seasonRegex.findall(url)
-	global seasonNumber
-	seasonNumber = matchObject
-	return seasonNumber[0]
 
 # -- Check if the file is not hidden.
 def is_hidden(filepath):
@@ -83,45 +146,5 @@ def renameFiles(rename):
 				print('File renamed from:',os.path.join(folderPath,filename),'to -> ',str(seasonNumber).zfill(2) + 'E' + str(counter + 1).zfill(2) + ' - ' + episodesNames[counter])
 			counter += 1
 
-
-def startScript():
-	print('Enter IMDB Series Url:')
-	url = input()
-	print('Downloading data from IMDB...')
-	ep = getSeriesUrl(url)
-	global seasonNumber
-	seasonNumber = getSeasonNumber(url)
-	# seasonNumber = seasonNum
-	print('These are the episodes names:\n')
-	for episode in ep:
-		print(episode)
-
-	print('\n')
-	print('Press Y to Continue.')
-	confirmation = input()
-
-	if(confirmation == 'Y' or confirmation == 'y'):
-		newWorkingDirectory()
-		renameFiles('n')
-		print('Do you want to continue? Type Y to continue or N to abort')
-		rename = input()
-
-		if(rename == 'Y' or rename == 'y'):
-			renameFiles(rename)
-
-		elif(rename == 'N' or rename == 'n'):
-			print('Script Terminated.')
-			quit()
-
-		else:
-			print('Please type Y or N.')
-
-
-	elif(confirmation == 'N' or confirmation == 'n'):
-		print('Script Terminated')
-		quit()
-
-	else:
-		print('Please type Y or N.')
 
 startScript()
